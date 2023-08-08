6623d0ea9c29c5ee694e6f4cd4522ef57c3dead8
tarie100 <tarie.school@gmail.com>
2023-08-08 05:44:59 -0700

    readme

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<stddef.h>
+#include<fcntl.h>
+#include<stdint.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<stdarg.h>
+
+
+
+#endif
